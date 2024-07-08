5264baf7d324efd6644c7e2924b753bb6b8be584
Memories2 <dothanhtai555@gmail.com>
2024-07-08 21:26:38 +0700

    video48

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -8,33 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
+@Controller
+public class UserController {
 
-
-
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello  from controller";
-//     }
-// }
-
-@RestController
-public class UserController{
-    
     private UserService userService;
-    
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-
-
-
-    @GetMapping("")
-    public String getMethodName() {
-        return this.userService.handleHello();
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "hello";
     }
-    
 }
+
+// @RestController
+// public class UserController{
+// private UserService userService;
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+// @GetMapping("")
+// public String getMethodName() {
+// return this.userService.handleHello();
+// }
+// }
