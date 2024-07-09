f2c6515595382ddb94a4e499a786ed87c4eaf145
Memories2 <dothanhtai555@gmail.com>
2024-07-09 12:49:10 +0700

    video55

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,12 +2,14 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
+
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class UserController {
@@ -26,13 +28,22 @@ public class UserController {
         return "hello";
     }
 
-    @RequestMapping("/admin/user") // day la ten mien
+    @RequestMapping("/admin/user") // day la ten mien // Ở đây chúng ta đang sử dụng method get
     public String getUserPage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("newUser", new User());
         model.addAttribute("hoidanit", "from controller width model");
         return "admin/user/create";
     }
+
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST) // đây là tên miền và ta đang nhận form với menthod = POST
+    public String createUser(Model model, @ModelAttribute ("newUser") User hoidanit) {
+        System.out.println("Run here " + hoidanit);
+       return "hello";
+    }
+    
+    
+
 }
 
 // @RestController
