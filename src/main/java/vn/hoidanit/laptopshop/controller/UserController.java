9986c2fd045bcfb7d4a48fd47c6bcad31e7e5bb8
Memories2 <dothanhtai555@gmail.com>
2024-07-10 10:42:10 +0700

    video61

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,16 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -22,27 +22,23 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("eric", "from controller width model");
         model.addAttribute("hoidanit", "from controller width model");
         return "hello";
     }
 
     @RequestMapping("/admin/user") // day la ten mien // Ở đây chúng ta đang sử dụng method get
     public String getUserPage(Model model) {
-        model.addAttribute("newUser", new User());
-        model.addAttribute("hoidanit", "from controller width model");
+        model.addAttribute("newUser", new User()); 
+        model.addAttribute("hoidanit", "from controller width model"); 
         return "admin/user/create";
     }
 
-
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST) // đây là tên miền và ta đang nhận form với menthod = POST
-    public String createUser(Model model, @ModelAttribute ("newUser") User hoidanit) {
-        System.out.println("Run here " + hoidanit);
-       return "hello";
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST) // đây là tên miền và ta đang nhận form // với menthod = POST
+    public String createUser(Model model, @ModelAttribute("newUser") User hoidanit) {
+        this.userService.handleSaveUser(hoidanit);
+        return "hello";
     }
-    
-    
 
 }
 
