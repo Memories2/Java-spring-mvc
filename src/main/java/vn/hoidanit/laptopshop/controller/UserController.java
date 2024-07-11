a2ed645f3c2f923f814f5714a2a0e01cd1f88f88
Memories2 <dothanhtai555@gmail.com>
2024-07-11 10:27:14 +0700

    video66

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
-import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class UserController {
@@ -27,30 +24,30 @@ public class UserController {
     public String getHomePage(Model model) {
         List<User> arrUsers = this.userService.getAllUserByEmail("1@gmail.com");
         System.out.println(arrUsers);
-
         model.addAttribute("eric", "from controller width model");
         model.addAttribute("hoidanit", "from controller width model");
         return "hello";
     }
 
-    @RequestMapping("/admin/user")
+
+    @RequestMapping("/admin/user") // hien thi danh sach nguoi dung
     public String getUserPage(Model model) {
-        return "admin/user/create";
+        List<User> users = this.userService.getAllUser();
+        model.addAttribute("users1", users);
+        return "admin/user/table-user";
     }
-    
 
     @RequestMapping("/admin/user/create") // day la ten mien // Ở đây chúng ta đang sử dụng method get
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
-        model.addAttribute("hoidanit", "from controller width model");
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST) // đây là tên miền và ta đang nhận form
-                                                                                // // với menthod = POST
+                                                                               // với menthod = POST
     public String createUser(Model model, @ModelAttribute("newUser") User hoidanit) {
-        this.userService.handleSaveUser(hoidanit); // 
-        return "hello";
+        this.userService.handleSaveUser(hoidanit); //
+        return "redirect:/admin/user";
     }
 
 }
