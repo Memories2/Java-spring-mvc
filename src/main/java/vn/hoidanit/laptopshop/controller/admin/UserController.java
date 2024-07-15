5bdd4f852aaa6e7cbb9372bc505949279b7aa0ba
Memories2 <dothanhtai555@gmail.com>
2024-07-15 09:58:52 +0700

    video92

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -1,23 +1,33 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletContext;
 
 @Controller
 public class UserController {
 
     private final UserService userService;
+    private final ServletContext servletContext;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ServletContext servletContext) {
         this.userService = userService;
+        this.servletContext = servletContext;
     }
 
     @RequestMapping("/")
@@ -75,22 +85,43 @@ public class UserController {
         return "admin/user/delete";
     }
 
-   @PostMapping("/admin/user/delete")
-   public String postDeleteUser(Model model, @ModelAttribute("newUser") User eric) {  //POST Xoa nguoi dung
+    @PostMapping("/admin/user/delete")
+    public String postDeleteUser(Model model, @ModelAttribute("newUser") User eric) { // POST Xoa nguoi dung
         this.userService.deleteAUser(eric.getId());
         return "redirect:/admin/user";
-   }
+    }
 
-    @RequestMapping("/admin/user/create") // day la ten mien // Ở đây chúng ta đang sử dụng method get - tao moi user
+    @GetMapping("/admin/user/create") // day la ten mien // ? ?ây chúng ta ?ang s? d?ng method get - tao moi user
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST) // đây là tên miền và ta đang nhận form
-                                                                               // với menthod = POST
-    public String createUser(Model model, @ModelAttribute("newUser") User hoidanit) {
-        this.userService.handleSaveUser(hoidanit); //
+    @PostMapping("/admin/user/create") // ?ây là tên mi?n và ta ?ang nh?n form
+                                       // với menthod = POST
+    public String createUser(Model model,
+            @ModelAttribute("newUser") User hoidanit,
+            @RequestParam("hoidanitFile") MultipartFile file) {
+
+        try {
+            byte[] bytes = file.getBytes();
+
+            String rootPath = this.servletContext.getRealPath("/resources/images");
+            File dir = new File(rootPath + File.separator + "avatar");
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            File serverFile = new File(dir.getAbsolutePath() +
+                    File.separator + +System.currentTimeMillis() + "-" +
+                    file.getOriginalFilename());
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // this.userService.handleSaveUser(hoidanit); //
         return "redirect:/admin/user";
     }
 
