e932b55a7dbc95de4fec76805fd32ee7d3038083
Memories2 <dothanhtai555@gmail.com>
2024-07-16 11:41:30 +0700

    video99

--- src/main/java/vn/hoidanit/laptopshop/domain/Role.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -2,7 +2,7 @@ package vn.hoidanit.laptopshop.domain;
 
 import java.util.List;
 
-import jakarta.annotation.Generated;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
