b9033beeade8af6810112fb12d475a1e6588d144
Memories2 <dothanhtai555@gmail.com>
2024-07-22 14:51:06 +0700

    video108

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemConTroller.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemConTroller.java
@@ -1,17 +1,33 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
 
 
 
 @Controller
 public class ItemConTroller {
 
+    private final ProductService productService;
+
+    public ItemConTroller (ProductService productService){
+        this.productService = productService;
+    }
+
+
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+
+        Product pr = this.productService.getProductById(id);
+        model.addAttribute("product", pr);
+        model.addAttribute("id",id);
+
         return "client/product/detail";
     }
     
